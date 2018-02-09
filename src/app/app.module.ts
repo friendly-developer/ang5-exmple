a0f3b395b9a0d34120288cf568b5beb740b20996
Varun G <gvsvarun@gmail.com>
2018-02-09 15:08:38 +0530

    feat: Added animations module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
