aac1094bd4845f1099cec27a867d8300c0e09138
Varun G <gvsvarun@gmail.com>
2018-02-09 16:22:24 +0530

    feat: Added animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,8 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
